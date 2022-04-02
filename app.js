5788272c4716ce0d48af5fab109c4110a806dfd7
Usta Eren Julian <erenjulian.usta@students.bfh.ch>
2022-04-01 19:43:56 -0600

    V2.0
    Version 2.0

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-new Vue({
+alert: function tipp() {
+    alert("Take a break and enjoy Monterrey!");
+}
 
+new Vue({
+    el: '#app',
+
+	data: {
+        message: true,
+    },
+
+    methods: {
+        
+    }
 });
+
